f60282e7e81ea9e936b3057ba4d8f569f8f2a58f
Mike <ramirezmike2004@gmail.com>
2026-05-06 16:46:11 +0200

    Auto-sync: file changes

---
+++ content.py
@@ -0,0 +1,20 @@
+import random 
+
+
+ideas = [
+    {
+        "title": Ëxplain what you're building",
+        "say":[
+            "what are you making?",
+            "why are you making it?",
+            "what problem does it solve?"
+        ],
+        "film": [
+            "laptop",
+            "typing",
+            "small progress"
+        ]
+
+    }
+
+]
